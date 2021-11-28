f8086b47fc2d38aeb701a85566bd6f96773eecbe
11111 <11111@DESKTOP-I2P1BG6>
2021-11-29 01:45:01 +0300

    Add project files.

---
+++ Lab_11(2)/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reshenie lineinogo uravneniy 0=kx+b");
+            try
+            {
+                Console.WriteLine("Vvedite znachenie b");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Vvedite znachenie k");
+                double n = Convert.ToDouble(Console.ReadLine());
+                if (n==0)
+                {
+                    Console.WriteLine("VVedeno necorrectnoe znachenie");
+                }
+                else
+                {
+                    Equation root = new Equation { b = a, k = n };
+                    root.Root();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("VVedeno necorrectnoe znachenie");
+            }
+            Console.ReadKey();
+        }
+        struct Equation
+        {
+            public double b, k;
+            public void Root()
+            {
+                Console.WriteLine("x={0:0.00}", -b / k);
+            }
+        }
+    }
+}
